8b33b525daaa9cf373e7155dd46115c29b6ed68b
Alexander Sparkowsky <info@roamingthings.de>
2023-08-06 14:03:13 +0200

    Initial commit

---
+++ test/codepipeline-github-deployment.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CodepipelineGithubDeployment from '../lib/codepipeline-github-deployment-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/codepipeline-github-deployment-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CodepipelineGithubDeployment.CodepipelineGithubDeploymentStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
