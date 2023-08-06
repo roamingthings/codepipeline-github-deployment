53a09383caa95d20bfd4f2232a84b0864b8e0a28
Alexander Sparkowsky <info@roamingthings.de>
2023-08-06 18:48:41 +0200

    feat: fetch process information

--- lambdas/create-deployment.ts
+++ lambdas/create-deployment.ts
@@ -1,3 +1,55 @@
+import {
+  CodePipelineClient,
+  GetPipelineCommand,
+  GetPipelineExecutionCommand,
+  GetPipelineExecutionInput,
+  GetPipelineInput
+} from "@aws-sdk/client-codepipeline";
+
 export const handler = async (event: any = {}): Promise<any> => {
   console.log('Received event:', JSON.stringify(event, null, 2));
+
+  const pipelineMessage = JSON.parse(event.Records[0].Sns.Message);
+  const region = pipelineMessage.region;
+  const pipelineName = pipelineMessage.detail.pipeline;
+  const pipelineExecutionId = pipelineMessage.detail['execition-id'];
+  const stage = pipelineMessage.detail.stage;
+  const state = pipelineMessage.detail.state;
+
+  const client = new CodePipelineClient({region});
+  const input: GetPipelineExecutionInput = {
+    pipelineName,
+    pipelineExecutionId,
+  };
+  const command = new GetPipelineExecutionCommand(input);
+  const response = await client.send(command);
+
+  const commitId = response.pipelineExecution?.artifactRevisions?.[0].revisionId;
+
+  console.log(JSON.stringify({
+    stage,
+    state,
+    commitId,
+  }))
+
+  const inputPipeline: GetPipelineInput = {
+    name: pipelineName,
+  }
+  const pipelineCommand = new GetPipelineCommand(inputPipeline);
+  const pipelineResponse = await client.send(pipelineCommand);
+
+  pipelineResponse.pipeline?.stages?.forEach(stage => {
+    stage.actions?.forEach(action => {
+      console.log(JSON.stringify({
+        name: action.name,
+        configuration: action.configuration,
+      }));
+      if (action.configuration?.FullRepositoryId) {
+        const fullRepositoryId = action.configuration?.FullRepositoryId
+        console.log(JSON.stringify({
+          fullRepositoryId,
+        }));
+      }
+    });
+  });
 };
